ccccec5e76f769815e7ff7e7882cca3a5c3a9aaa
Evan Senter (614925) <614925@swl.local>
2024-09-25 08:19:58 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -10,14 +10,22 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         System.out.println("What is your product price?");
-        productPrice = scan.nextDouble();
 
-        if (productPrice >= freeShipping)
-            System.out.println("Shipping is free");
-        else {
-            productShipping = productPrice * shippingPrice;
-            productTotal = productPrice + productShipping;
-            System.out.println("The price of your product with shipping is " + productTotal + "and your shipping cost is " + productShipping);
+        if (scan.hasNextDouble()) {
+            productPrice = scan.nextDouble();
+
+
+            if (productPrice >= freeShipping)
+                System.out.println("Shipping is free");
+            else {
+                productShipping = productPrice * shippingPrice;
+                productTotal = productPrice + productShipping;
+                System.out.println("The price of your product with shipping is " + productTotal + " and your shipping cost is " + productShipping);
+            }
         }
+
+        else
+            System.out.println("Sorry you have entered the wrong data type");
+
     }
 }
