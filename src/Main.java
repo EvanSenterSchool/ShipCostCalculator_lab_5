4555eb92f131ed3a5b1e43df5013c28d5daf4e15
Evan Senter (614925) <614925@SWL.local>
2024-09-25 08:19:58 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double productPrice;
+        double  shippingPrice = 0.02;
+        double productShipping;
+        double productTotal;
+        int freeShipping =100;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your product price?");
+        productPrice = scan.nextDouble();
+
+        if (productPrice >= freeShipping)
+            System.out.println("Shipping is free");
+        else {
+            productShipping = productPrice * shippingPrice;
+            productTotal = productPrice + productShipping;
+            System.out.println("The price of your product with shipping is " + productTotal + "and your shipping cost is " + productShipping);
+        }
+    }
+}
